21cf53caa201c465cc284a84b510d8287381c71a
Venkatesh Naidu <VenkateshNaidu@truckstop.com>
2020-01-24 18:28:28 -0600

    Latest

--- popup.js
+++ popup.js
@@ -1,37 +1,54 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// // Copyright 2018 The Chromium Authors. All rights reserved.
+// // Use of this source code is governed by a BSD-style license that can be
+// // found in the LICENSE file.
 
-'use strict';
+// 'use strict';
 
-let changeColor = document.getElementById('qa-button');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-  changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-  };
-});
+// let changeColor = document.getElementById('qa-button');
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+//   changeColor.onclick = function(element) {
+//     let color = element.target.value;
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       chrome.tabs.executeScript(
+//           tabs[0].id,
+//           {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//     });
+//   };
+// });
 
 // var request = new XMLHttpRequest()
-
+// console.log("Line 22")
 // request.open('GET', 'https://app.truckstop.com', true)
 // request.onload = function() {
 //   // Begin accessing JSON data here
-//   var statusCode = context.response.meta.statusCode;
-
-//   if (statusCode >= 200 && statusCode < 400) {
-    
-//       console.log(statusCode && 'UP')
-    
+//   var data = JSON.parse(this.response)
+// console.log("Line27")
+//   if (request.status >= 200 && request.status < 400) {
+//     console.log('Good')
 //   } else {
-//     console.log(statusCode && 'DOWN')
+//     console.log('error')
 //   }
 // }
 
 // request.send()
+const axios = require('axios');
+
+
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+
+
